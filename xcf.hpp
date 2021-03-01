85e54830b9f2d71a4a5afe1701e7797e5a6bad31
Rick Wertenbroek <rick.wertenbroek@unil.ch>
2021-03-01 09:34:09 +0100

    Added copyright

--- xcf.hpp
+++ xcf.hpp
@@ -1,3 +1,25 @@
+/*******************************************************************************
+ * Copyright (C) 2021 Rick Wertenbroek, University of Lausanne
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
+
 #ifndef __XCF_HPP__
 #define __XCF_HPP__
 
@@ -262,6 +284,8 @@ bool matrices_differ(std::string f1, std::string f2) {
     if (m1.size() != m2.size()) {
         std::cerr << "Different outer size" << std::endl;
         return true;
+    } else {
+        std::cerr << "Outer sizes are the same : " << m1.size() << std::endl;
     }
 
     for (size_t i = 0; i < m1.size(); ++i) {
