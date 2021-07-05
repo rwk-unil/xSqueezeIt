bc2d9183066b99df964ff1dc00b9c1a426fa1cc2
Rick Wertenbroek <rick.wertenbroek@unil.ch>
2021-07-05 10:43:42 +0200

    Separated bcf_traversal in .cpp and .hpp

--- bcf_traversal.hpp
+++ bcf_traversal.hpp
@@ -37,26 +37,7 @@ public:
 
     // Todo set region / samples
 
-    void traverse(const std::string filename) {
-        initialize_bcf_file_reader(bcf_fri, filename);
-
-        handle_bcf_file_reader();
-        while(bcf_next_line(bcf_fri)) {
-            // Unpack the line and get genotypes
-            bcf_unpack(bcf_fri.line, BCF_UN_STR);
-            int ngt = bcf_get_genotypes(bcf_fri.sr->readers[0].header, bcf_fri.line, &(bcf_fri.gt_arr), &(bcf_fri.ngt_arr));
-            int line_max_ploidy = ngt / bcf_fri.n_samples;
-
-            // Check ploidy, only support diploid for the moment
-            if (line_max_ploidy != PLOIDY) {
-                std::cerr << "[ERROR] Ploidy of samples is different than 2" << std::endl;
-                exit(-1); // Change this
-            }
-
-            handle_bcf_line();
-        }
-        destroy_bcf_file_reader(bcf_fri);
-    }
+    void traverse(const std::string filename);
 
     virtual ~BcfTraversal() {}
 
@@ -76,47 +57,11 @@ public:
     virtual void transform_header() {}
     virtual void transform_record() {}
 
-    void transform(const std::string& ifname, const std::string& ofname) {
-        fp = hts_open(ofname.c_str(), ofname.compare("-") ? "wb" : "wu"); // "-" for stdout
-        if (fp == NULL) {
-            std::cerr << "Could not open " << ofname << std::endl;
-            throw "File open error";
-        }
-
-        traverse(ifname);
-
-        // Close / Release ressources
-        hts_close(fp);
-        bcf_hdr_destroy(hdr);
-    }
+    void transform(const std::string& ifname, const std::string& ofname);
 
 protected:
-    void handle_bcf_file_reader() override {
-        // Duplicate the header from the input bcf
-        hdr = bcf_hdr_dup(bcf_fri.sr->readers[0].header);
-
-        transform_header();
-
-        // Write the header to the new file
-        int ret = bcf_hdr_write(fp, hdr);
-        if (ret) {
-            std::cerr << "Failed to write header" << std::endl;
-            exit(-1);
-        }
-    }
-
-    void handle_bcf_line() override {
-        rec = bcf_fri.line;
-
-        transform_record();
-        bcf_update_genotypes(hdr, rec, bcf_fri.gt_arr, bcf_hdr_nsamples(hdr) * PLOIDY);
-
-        int ret = bcf_write1(fp, hdr, rec);
-        if (ret) {
-            std::cerr << "Failed to write record" << std::endl;
-            exit(-1);
-        }
-    }
+    void handle_bcf_file_reader() override;
+    void handle_bcf_line() override;
 
     bcf1_t *rec;
     htsFile* fp;
@@ -134,19 +79,7 @@ public:
     }
 
 protected:
-    void transform_record() override {
-        for (size_t i = 0; i < bcf_fri.n_samples; ++i) {
-            int32_t allele_0 = bcf_gt_allele(bcf_fri.gt_arr[i*2]);
-            int32_t allele_1 = bcf_gt_allele(bcf_fri.gt_arr[i*2+1]);
-            if (distrib(gen) == 1) {
-                bcf_fri.gt_arr[i*2] = bcf_gt_unphased(allele_0);
-                bcf_fri.gt_arr[i*2+1] = bcf_gt_unphased(allele_1);
-            } else {
-                bcf_fri.gt_arr[i*2] = bcf_gt_unphased(allele_1);
-                bcf_fri.gt_arr[i*2+1] = bcf_gt_unphased(allele_0);
-            }
-        }
-    }
+    void transform_record() override;
 
     std::random_device rd;  // Will be used to obtain a seed for the random number engine
     std::mt19937 gen; // Standard mersenne_twister_engine seeded with rd()
