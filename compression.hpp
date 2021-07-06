332dd34f521958d85b8c6b2cb7a8b82ce8426b2a
Rick Wertenbroek <rick.wertenbroek@unil.ch>
2021-07-06 12:46:29 +0200

    Commented header

--- compression.hpp
+++ compression.hpp
@@ -191,27 +191,29 @@ constexpr uint8_t PLOIDY_DEFAULT = 2;
 // } special_bits_t;
 
 struct header_s {
+    // "rsvd" fields are "reserved", unused for the moment and kept for future additions
+
     // 32 bytes
-    uint32_t endianness = ENDIANNESS;
-    uint32_t first_magic = MAGIC;
-    uint32_t version = VERSION;
-    uint8_t  ploidy = PLOIDY_DEFAULT;
-    uint8_t  ind_bytes = 0;
-    uint8_t  aet_bytes = 0;
-    uint8_t  wah_bytes = 0;
+    uint32_t endianness = ENDIANNESS; // Endianness sanity check
+    uint32_t first_magic = MAGIC;     // File format sanity check
+    uint32_t version = VERSION;       // File format version
+    uint8_t  ploidy = PLOIDY_DEFAULT; // Ploidy of samples encoded
+    uint8_t  ind_bytes = 0;           // Number of bytes used to save indices
+    uint8_t  aet_bytes = 0;           // Number of bytes used to save positions
+    uint8_t  wah_bytes = 0;           // Number of bytes used for WAH
     union {
         uint8_t special_bitset = 0;
         struct {
-            bool has_missing : 1;
-            bool non_uniform_phasing : 1;
+            bool has_missing : 1;     // The input file had missing data
+            bool non_uniform_phasing : 1; // The input file has phased / unphased mixed data
             uint8_t rsvd__1 : 6;
         };
     };
     union {
         uint8_t specific_bitset = 0;
         struct {
-            bool iota_ppa : 1;
-            bool no_sort : 1;
+            bool iota_ppa : 1;        // Reset sort instead of saving permutation arrays
+            bool no_sort : 1;         // Data is not permutated
             uint8_t rsvd__2 : 6;
         };
     };
@@ -219,33 +221,34 @@ struct header_s {
     uint32_t rsvd_1[3] = {0,};
 
     // 64 bytes
-    uint64_t hap_samples = 0;
-    uint64_t num_variants = 0;
-    uint32_t block_size = 0;
-    uint32_t number_of_blocks = 0;
-    uint32_t ss_rate = 0;
-    uint32_t number_of_ssas = 0;
-    uint32_t indices_offset = 0;
-    uint32_t ssas_offset = 0;
-    uint32_t wahs_offset = 0;
-    uint32_t samples_offset = 0;
-    uint32_t indices_sparse_offset = 0;
+    uint64_t hap_samples = 0;         // Number of haplotypes
+    uint64_t num_variants = 0;        // Number of variants (total number of ALTs)
+    uint32_t block_size = 0;          // DEPRECATED
+    uint32_t number_of_blocks = 0;    // DEPRECATED
+    uint32_t ss_rate = 0;             // Sub Sample rate of permutation arrays / reset sort rate
+    // Offsets, positions of data in the binary file
+    uint32_t number_of_ssas = 0;      // Number of sampled loci for random access = ceil(num_variants/ss_rate)
+    uint32_t indices_offset = 0;      // Position in the binary file of WAH indices
+    uint32_t ssas_offset = 0;         // Position in the binary file of sub sampled permutation arrays (if any)
+    uint32_t wahs_offset = 0;         // Position in the binary file of WAH data
+    uint32_t samples_offset = 0;      // Position in the binary file of samples (e.g., "NA12878", "HG00101")
+    uint32_t indices_sparse_offset = 0; // Position in the binary file of indices for the sparse data
     uint32_t rsvd_offset = 0;
-    uint32_t rearrangement_track_offset = 0;
-    uint32_t sparse_offset = 0;
+    uint32_t rearrangement_track_offset = 0; // Position in the binary file of the rearrangement track
+    uint32_t sparse_offset = 0;       // Position in the binary file of the sparse data
 
     // 128 bytes
-    uint32_t rare_threshold = 0;
-    uint64_t xcf_entries = 0;
+    uint32_t rare_threshold = 0;      // Threshold for the rearrangement track / sorting / wah vs sparse
+    uint64_t xcf_entries = 0;         // Num entries in the BCF file (may be less than num_variants if multi-allelic)
     uint8_t rsvd_3[116] = {0,};
 
     // 32 bytes
     uint32_t rsvd_4[3] = {0,};
-    uint32_t sample_name_chksum = 0;;
-    uint32_t bcf_file_chksum = 0;
-    uint32_t data_chksum = 0;
-    uint32_t header_chksum = 0;
-    uint32_t last_magic = MAGIC;
+    uint32_t sample_name_chksum = 0;; // Checksum unused for now
+    uint32_t bcf_file_chksum = 0;     // Checksum unused for now
+    uint32_t data_chksum = 0;         // Checksum unused for now
+    uint32_t header_chksum = 0;       // Checksum unused for now
+    uint32_t last_magic = MAGIC;      // Sanity check magic
 } __attribute__((__packed__));
 
 typedef struct header_s header_t;
