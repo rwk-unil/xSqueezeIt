aca24f0c82e37a1e18d9ba983c5436f1524425e8
Rick Wertenbroek <rick.wertenbroek@unil.ch>
2021-11-17 11:24:54 +0100

    Added include for basename()

--- include/fs.hpp
+++ include/fs.hpp
@@ -1,10 +1,21 @@
 #ifndef __FS_HPP__
 #define __FS_HPP__
 
+#include <libgen.h> // Has dirname() / basename()
+/*
+The functions dirname() and basename() break a null-terminated pathname string
+into directory and filename components. In the usual case, dirname() returns the
+string up to, but not including, the final '/', and basename() returns the
+component following the final '/'. Trailing '/' characters are not counted as
+part of the pathname.
+*/
+
 #if __cplusplus >= 201703L
     #include <filesystem>
     namespace fs = std::filesystem;
     using fs::remove;
+    // This can be used instead of basename() (more portable, if C++17 is available)
+    // fs::path( "/foo/bar.txt" ).filename() => "bar.txt"
 #else
     #include <stdio.h> // Has remove()
     #include <sys/stat.h>
