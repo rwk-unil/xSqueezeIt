e9f757c452b622259782d9b0082c4e735e235c49
Rick Wertenbroek <rick.wertenbroek@unil.ch>
2022-12-16 11:21:20 +0100

    Minor work on rare genotype class

--- include/rare_genotype.h
+++ include/rare_genotype.h
@@ -24,13 +24,13 @@
 #define _RARE_GENOTYPE_H
 
 #define SETBIT(n,i)	(n)|=(1UL<<(i));
-#define CLRBIT(n,i)	(n)&=~(1UL<<i);
+#define CLRBIT(n,i)	(n)&=~(1UL<<(i));
 #define GETBIT(n,i)	(((n)>>(i))&1U);
 
 
 class rare_genotype {
 public:
-
+	/** @note bit order is implementation dependent, so not the best idea to write a file */
 	unsigned int idx : 27;	//Individual index of the non Major/Major genotype
 	unsigned int het : 1;	//Is it het?
 	unsigned int mis : 1;	//Is it missing?	If none of the two, it is then Minor/Minor
@@ -67,6 +67,82 @@ public:
 		al0 = GETBIT(value, 2);
 		al1 = GETBIT(value, 1);
 		pha = GETBIT(value, 0);
+	}
+};
+
+class rare_genotype2 {
+public:
+	uint32_t idx : 27;
+	uint32_t al0 : 2; /* In BCF format (without phase bit) = bits 1 and 2 of BCF "int" */
+	uint32_t al1 : 2; /* In BCF format (without phase bit) = bits 1 and 2 of BCF "int" */
+	uint32_t pha : 1;
+
+	/**
+	 * @brief Convert BCF "int" allele to 2 bit representation (bit 1 and 2 only of bits 0 to 31)
+	 *        Therefore can only handle "missing", "ref", "alt1", "alt2" alleles types
+	 *        Only "missing", "ref", and "alt" ("alt1") are used here
+	 *        Note: The 32-bit BCF format has bit 0 for phasing and can represent more alts
+	 *
+	 * @param bcf_binary_allele
+	 * @return uint32_t
+	 */
+	inline uint32_t bcf_binary_allele_to_2bit(int32_t bcf_binary_allele) {
+		return (bcf_binary_allele >> 1) & 0x3;
+	}
+
+	rare_genotype2() :
+		idx(0), al0(0), al1(0), pha(0)
+	{}
+
+	rare_genotype2(uint32_t idx, int32_t al0, int32_t al1) :
+		idx(idx), al0(bcf_binary_allele_to_2bit(al0)), al1(bcf_binary_allele_to_2bit(al1)),
+		pha(bcf_gt_is_phased(al0) | bcf_gt_is_phased(al1)) /* usually the bit is set only on allele 1 (see VCF spec) */
+	{}
+
+	/**
+	 * @brief Serializes to uint32_t (careful endianness sensitive ! (note that XSI checks endianness))
+	 *
+	 * @return uint32_t
+	 */
+	uint32_t get() {
+		/* Serialize */
+		uint32_t val = idx;
+		val = (val << 2) | (al0 & 0x3);
+		val = (val << 2) | (al1 & 0x3);
+		val = (val << 1) | (pha & 0x1);
+
+		return val;
+	}
+
+	/**
+	 * @brief Deserializes from uint32_t (careful endianness sensitive ! (note that XSI checks endianness))
+	 *
+	 * @param val
+	 */
+	void set(uint32_t val) {
+		/* Deserialize (mirror of the above) */
+		pha = val & 0x1;
+		al1 = (val >> 1) & 0x3;
+		al0 = (val >> 3) & 0x3;
+		idx = (val >> 5) & 0x07FFFFFF;
+	}
+
+	/**
+	 * @brief Get allele 0 as BCF "int" representation (handles missing, and has phase bit)
+	 *
+	 * @return int32_t
+	 */
+	inline int32_t get_allele0_bcf() {
+		return int32_t(al0) << 1;
+	}
+
+	/**
+	 * @brief Get allele 1 as BCF "int" representation (handles missing, and has phase bit)
+	 *
+	 * @return int32_t
+	 */
+	inline int32_t get_allele1_bcf() {
+		return (int32_t(al1) << 1) | pha;
 	}
 };
 
